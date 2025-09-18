89a661c83afd1fb033cd4ba722619ffd89ec39f2
Евгения Андерлин <evgeniaanderlin@MacBook-Air-Evgenia-2.local>
2025-09-18 12:40:35 +0500

    Добавила страницы Portfolio и Contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>Напишите мне на email: evgeniaanderlin@gmail.com</p>
+      </div>
+    );
+  }
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export default function PortfolioPage() {
+    return (
+      <div>
+        <h1>My Portfolio</h1>
+        <p>Здесь будут мои проекты 🎨</p>
+      </div>
+    );
+  }
